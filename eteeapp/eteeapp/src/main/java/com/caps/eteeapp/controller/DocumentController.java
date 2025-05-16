46cf245d34628dee18dd9ae7e5de140abb393c33
Ciddy Macapinlac <zed.maxcapinlac@gmail.com>
2025-05-16 11:35:19 +0800

    Added api in DocumentController.java

--- eteeapp/eteeapp/src/main/java/com/caps/eteeapp/controller/DocumentController.java
+++ eteeapp/eteeapp/src/main/java/com/caps/eteeapp/controller/DocumentController.java
@@ -1,22 +1,31 @@
 package com.caps.eteeapp.controller;
 
-import com.caps.eteeapp.model.Document;
-import com.caps.eteeapp.model.FileResponse;
-import com.caps.eteeapp.service.DocumentService;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.caps.eteeapp.model.Document;
+import com.caps.eteeapp.model.FileResponse;
+import com.caps.eteeapp.service.DocumentService;
+
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -65,6 +74,27 @@ public class DocumentController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Resource> downloadFile(@PathVariable Long id, HttpServletRequest request) {
+        Resource resource = documentService.getDocumentFile(id);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    // Add this endpoint for /download/{id}
+    @GetMapping("/download/{id}")
+    public ResponseEntity<Resource> downloadDocument(@PathVariable Long id, HttpServletRequest request) {
         Resource resource = documentService.getDocumentFile(id);
 
         String contentType = null;
